84212e808d4b50f51c1e7c8bf89c5422ad7d4327
admin <admin@DESKTOP-FKR8P39>
2025-03-13 16:23:39 +0530

    ist commit

---
+++ ECOMMERCE/src/test/java/practices/Shadow.java
@@ -0,0 +1,24 @@
+package practices;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.SearchContext;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Shadow {
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver d=new ChromeDriver();
+		d.manage().window().maximize();
+		d.get("https://demoapps.qspiders.com/ui?scenario=1");
+		Thread.sleep(1000);
+		d.findElement((By.xpath("//section[contains(text(),'Shadow')][1]"))).click();
+		Thread.sleep(1000);
+		d.findElement((By.xpath("//section[contains(text(),'Shadow')][1]"))).click();
+		Thread.sleep(2000);
+		SearchContext shadow_host = d.findElement(By.xpath("//form/div[1]")).getShadowRoot();
+		//Thread.sleep(1000);
+		shadow_host.findElement(By.cssSelector("//input[@type='text']")).sendKeys("MADHURA");
+		
+
+}
+}
